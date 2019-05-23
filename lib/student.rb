90260e7a7ef77a4b602bcbcafdf16b0cc2690947
Emily Bolten <emilybolten@gmail.com>
2019-05-23 12:19:42 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,43 @@
+require 'sqlite3'
+require_relative '../lib/student'
+require 'pry'
+ 
+DB = {:conn => SQLite3::Database.new("db/students.db")}
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    return new_student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    SQL
+
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
   end
 
-  def self.find_by_name(name)
+  def self.find_by_name(new_name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+    SELECT * FROM students WHERE name = ?
+    SQL
+    x = DB[:conn].execute(sql, new_name)[0]
+
+    self.new_from_db(x)
   end
   
   def save
@@ -40,4 +65,61 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = 9
+    SQL
+    
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT * FROM students WHERE NOT grade = 12
+    SQL
+    
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = 10
+    SQL
+
+    array = []
+    DB[:conn].execute(sql).collect do |row|
+      array << self.new_from_db(row)
+    end
+    return array[0..num-1]
+  end
+  
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = 10
+    SQL
+    
+    array = []
+    DB[:conn].execute(sql).each do |row|
+      array << self.new_from_db(row)
+    end
+    return array[0]
+  end
+
+  def self.all_students_in_grade_X(num)
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = ?
+    SQL
+    
+    DB[:conn].execute(sql, num).each do |row|
+      self.new_from_db(row)
+    end
+  end
+
+
+
 end
